4513e5c2cecc911c20893ca778935f734b3ef282
dcortinez93 <45173309+dcortinez93@users.noreply.github.com>
2021-10-10 20:01:40 -0300

    Update request_q4.py

--- Zoomcamp Homework #5/request_q4.py
+++ Zoomcamp Homework #5/request_q4.py
@@ -5,9 +5,7 @@ Created on Sun Oct 10 17:56:59 2021
 @author: Diego Cortínez
 """
 import requests
-
 url = "http://localhost:9696/predict"
 customer = {"contract": "two_year", "tenure": 1, "monthlycharges": 10}
-
 res = requests.post(url, json=customer).json()
 print(res)
